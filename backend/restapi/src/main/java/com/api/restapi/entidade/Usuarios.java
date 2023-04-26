1fcbbf2dba4092917243a870c902295981a5dff9
Diego Oliveira <diegolvr14@gmail.com>
2023-04-26 16:56:50 -0300

    feature: Adicionando o frontend.

--- backend/restapi/src/main/java/com/api/restapi/entidade/Usuarios.java
+++ backend/restapi/src/main/java/com/api/restapi/entidade/Usuarios.java
@@ -16,28 +16,20 @@ public class Usuarios {
     @Column(nullable = false, name = "nome", length = 90)
     private String nome;
 
-    @Column(nullable = false, name = "endereco", columnDefinition = "LONGTEXT")
-    private String endereco;
-
-    @Column(nullable = false, name = "telefone", length = 20)
-    private String telefone;
-
     @Column(nullable = false, name = "email", length = 45)
     private String email;
 
-    @Column(name = "outros_detalhes", columnDefinition = "LONGTEXT")
-    private String outros_detalhes;
+    @Column(nullable = false, name = "senha", length = 45)
+    private String senha;
 
     public Usuarios() {
     }
 
-    public Usuarios(Long id, String nome, String endereco, String telefone, String email, String outros_detalhes) {
+    public Usuarios(Long id, String nome, String email, String senha) {
         this.id = id;
         this.nome = nome;
-        this.endereco = endereco;
-        this.telefone = telefone;
         this.email = email;
-        this.outros_detalhes = outros_detalhes;
+        this.senha = senha;
     }
 
     public Long getId() {
@@ -56,21 +48,6 @@ public class Usuarios {
         this.nome = nome;
     }
 
-    public String getEndereco() {
-        return endereco;
-    }
-
-    public void setEndereco(String endereco) {
-        this.endereco = endereco;
-    }
-
-    public String getTelefone() {
-        return telefone;
-    }
-
-    public void setTelefone(String telefone) {
-        this.telefone = telefone;
-    }
 
     public String getEmail() {
         return email;
@@ -80,12 +57,12 @@ public class Usuarios {
         this.email = email;
     }
 
-    public String getOutros_detalhes() {
-        return outros_detalhes;
+    public String getSenha() {
+        return senha;
     }
 
-    public void setOutros_detalhes(String outros_detalhes) {
-        this.outros_detalhes = outros_detalhes;
+    public void setSenha(String senha) {
+        this.senha = senha;
     }
 
 }
